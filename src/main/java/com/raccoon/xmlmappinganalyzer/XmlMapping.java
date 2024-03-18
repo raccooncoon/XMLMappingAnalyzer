88ca846ffcdca437f4a8135f47e386f1878e52b5
임민영 <raccoon@shinsegae.com>
2024-03-19 03:09:44 +0900

    init

---
+++ src/main/java/com/raccoon/xmlmappinganalyzer/XmlMapping.java
@@ -0,0 +1,13 @@
+package com.raccoon.xmlmappinganalyzer;
+
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import org.jetbrains.annotations.NotNull;
+
+public class XmlMapping extends AnAction {
+
+    @Override
+    public void actionPerformed(@NotNull AnActionEvent e) {
+
+    }
+}
