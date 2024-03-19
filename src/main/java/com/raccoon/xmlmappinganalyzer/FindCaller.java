97815a73bc11694ab6e079f13b22c7c8cb8df63a
임민영 <raccoon@shinsegae.com>
2024-03-19 12:28:48 +0900

    불필요 함수 제거

--- src/main/java/com/raccoon/xmlmappinganalyzer/FindCaller.java
+++ src/main/java/com/raccoon/xmlmappinganalyzer/FindCaller.java
@@ -1,26 +1,14 @@
 package com.raccoon.xmlmappinganalyzer;
 
 import com.intellij.psi.PsiMethod;
-import com.intellij.psi.PsiReference;
 import com.intellij.psi.search.searches.ReferencesSearch;
 import com.intellij.psi.util.PsiTreeUtil;
-import com.intellij.util.Query;
 
 import java.util.HashSet;
-import java.util.Optional;
 import java.util.Set;
 
+
 public class FindCaller  {
-    //메소드를 호출 하는 최상위 메소드 단 건 검색
-    private PsiMethod findTopCallingMethod(PsiMethod method) {
-        return Optional.ofNullable(method)
-                .map(ReferencesSearch::search)
-                .map(Query::findFirst)
-                .map(PsiReference::getElement)
-                .map(element -> PsiTreeUtil.getParentOfType(element, PsiMethod.class))
-                .map(this::findTopCallingMethod)
-                .orElse(method);
-    }
 
     //메소드를 호출 하는 최상위 메소드 모두 검색
     public Set<PsiMethod> findTopCallingMethods(PsiMethod method) {
